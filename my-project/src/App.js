62ee6ae67c23fb45085fd7e8e9906a6bc9d3f817
Danil Bronnikov <bronnikovdanil@gmail.com>
2024-04-20 22:31:40 +0100

    Upload all the changes to git

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
-import Home from '//pages/Home';
+import Home from './/pages/Home';
 import Analysis from './/pages/Analysis';
 import Upload from './/pages/Upload';
 
