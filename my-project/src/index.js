905003eff025fa53078383ac9532b7b1c14f8db0
DanilBV04 <bronnikovdanil@gmail.com>
2024-04-22 00:36:25 +0100

    Update react-router-dom to version 6.22.3

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -11,11 +11,7 @@ root.render(
   </React.StrictMode>
 );
 
-function Greeting(props) {
-  return <h1>Hello, this is the first line of the project
-    but it sure ain't the last one, [props.name]!
-  </h1>
-}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
