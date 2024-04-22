fea10612b75256485a49f3d6da795bdd5aff619e
DanilBV04 <bronnikovdanil@gmail.com>
2024-04-22 13:18:59 +0100

    Yes

--- my-project/src/Navbar.js
+++ my-project/src/Navbar.js
@@ -1,51 +1,77 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import { IoClose, IoMenu } from 'react-icons/io5';
-import './Navbar.css';
-
-const Navbar = () => {
-    const [showMenu, setShowMenu] = useState(false);
-
-    const toggleMenu = () => {
-        setShowMenu(!showMenu);
-    };
-
-    const closeMenuOnMobile = () => {
-        if (window.innerWidth <= 1150) {
-            setShowMenu(false);
-        }
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { IoClose, IoMenu } from "react-icons/io5";
+import { useMediaQuery } from "react-responsive";
+import "./Navbar.css";
+const NavbarHook = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const isMobile = useMediaQuery({ maxWidth: "1150px" });
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  const closeMobileMenu = () => {
+    if (isMobile) {
+      setIsMenuOpen(false);
     }
+  };
+  const renderNavLinks = () => {
+    const listClassName = isMobile ? "nav__list" : "nav__list__web";
+    const linkClassName = "nav__link";
+    const buttonClassName = "nav__cta";
     return (
-        <header className='header'>
-            <nav className='nav container'>
-                <div className={"nav__menu"} id="nav-menu">
-         <ul className="nav__list">
-           <li className="nav__item">
-             <NavLink to="/" className="nav__link">
-               Home
-             </NavLink>
-           </li>
-           <li className="nav__item">
-             <NavLink to="/news" className="nav__link">
-               Analysis
-             </NavLink>
-           </li>
-           <li className="nav__item">
-             <NavLink
-               to="/about-us"
-               className="nav__link"
-             >
-               Upload
-             </NavLink>
-           </li>
-           </ul>
-       </div>
-       <div className="nav__toggle" id="nav-toggle">
-         <IoMenu />
-       </div>
-     </nav>
-   </header>
- );
+      <ul className={listClassName}>
+        <li>
+          <NavLink to="/" className={linkClassName} onClick={closeMobileMenu}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/Analysis"
+            className={linkClassName}
+            onClick={closeMobileMenu}
+          >
+            Analysis
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/Upload"
+            className={linkClassName}
+            onClick={closeMobileMenu}
+          >
+            Upload
+          </NavLink>
+        </li>
+      </ul>
+    );
+  };
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <NavLink to="/" className="nav__logo">
+          Navigation Bar
+        </NavLink>
+        {isMobile && (
+          <div className="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
+            <IoMenu />
+          </div>
+        )}
+        {isMobile ? (
+          <div
+            className={`nav__menu  ${isMenuOpen ? "show-menu" : ""}`}
+            id="nav-menu"
+          >
+            {renderNavLinks()}
+            <div className="nav__close" id="nav-close" onClick={toggleMenu}>
+              <IoClose />
+            </div>
+          </div>
+        ) : (
+          renderNavLinks()
+        )}
+      </nav>
+    </header>
+  );
 };
-
-export default Navbar;
+export default NavbarHook;
